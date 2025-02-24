aa538bf0a1a04d126cc05c0118287836639245fc
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2025-02-24 09:47:35 +0530

    practice

--- script.js
+++ script.js
@@ -1,103 +1,65 @@
-// let brandSwiper = document.querySelector(".brandSwiper");
+document.addEventListener("DOMContentLoaded", function () {
+    let brandSwiper = new Swiper(".brandSwiper", {
+        slidesPerView: 'auto',
+        loop: true,
+        speed: 5000,
+        autoplay: { delay: 0 },
+    });
 
-let brandSwiper = new Swiper(".brandSwiper", {
-    slidesPerView: 'auto',
+    let showCaseDown = new Swiper(".showCaseSwiperDown", {
+        slidesPerView: 2,
+        spaceBetween: 29,
+        loop: true,
+        speed: 5000,
+        direction: "vertical",
+        autoplay: { delay: 0, reverseDirection: true },
+    });
 
-    loop: true,
-    speed: 5000,
+    let showCaseUp = new Swiper(".showCaseSwiperUp", {
+        slidesPerView: 2,
+        spaceBetween: 29,
+        loop: true,
+        speed: 5000,
+        direction: "vertical",
+        autoplay: { delay: 0 },
+    });
 
-    autoplay: {
-        delay: 0,
-    },
+    let testimonialSwiper = new Swiper(".testimonialSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 85,
+        loop: true,
+        speed: 5000,
+        autoplay: { delay: 0 },
+    });
 
-})
+    let projectsSwiper = new Swiper(".projectsSwiper", {
+        slidesPerView: 'auto',
+        loop: true,
+        speed: 3000,
+        autoplay: { delay: 0 },
+    });
 
+    let portfolioSwiper1 = new Swiper(".portfolioSwiper1", {
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 10,
+        speed: 8000,
+        autoplay: { delay: 0 },
+    });
 
-let showCaseDown = new Swiper(".showCaseSwiperDown", {
+    let portfolioSwiper2 = new Swiper(".portfolioSwiper2", {
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 10,
+        speed: 8000,
+        autoplay: { delay: 0, reverseDirection: true },
+    });
 
-    slidesPerView: 'auto',
-    spaceBetween: 29,
-    loop: true,
-    speed: 5000,
-    direction: "vertical",
-
-    autoplay: {
-        delay: 0,
-        reverseDirection: true,
-    },
-})
-
-
-
-let showCaseUp = new Swiper(".showCaseSwiperUp", {
-
-    slidesPerView: 'auto',
-
-    spaceBetween: 29,
-    loop: true,
-    speed: 5000,
-    direction: "vertical",
-
-    autoplay: {
-        delay: 0,
-
-    },
-})
-
-
-let testimonialSwiper = new Swiper(".testimonialSwiper", {
-    slidesPerView: 'auto',
-    spaceBetween: 85,
-    loop: true,
-    speed: 5000,
-
-
-    autoplay: {
-        delay: 0,
-    },
-})
-
-let projectsSwiper = new Swiper(".projectsSwiper", {
-    slidesPerView: 'auto',
-    loop: true,
-    speed: 3000,
-
-    autoplay: {
-        delay: 0,
-    },
-})
-
-let portfolioSwiper1 = new Swiper(".portfolioSwiper1", {
-    slidesPerView: 'auto',
-    loop: true,
-    spaceBetween: 10,
-    speed: 8000,
-
-    autoplay: {
-        delay: 0,
-    },
-})
-
-let portfolioSwiper2 = new Swiper(".portfolioSwiper2", {
-    slidesPerView: 'auto',
-    loop: true,
-    spaceBetween: 10,
-    speed: 8000,
-
-    autoplay: {
-        delay: 0,
-        reverseDirection: true,
-    },
-})
-
-let portfolioSwiper3 = new Swiper(".portfolioSwiper3", {
-    slidesPerView: 'auto',
-    loop: true,
-    spaceBetween: 10,
-    speed: 8000,
-
-    autoplay: {
-        delay: 0,
-
-    },
-})
+    let portfolioSwiper3 = new Swiper(".portfolioSwiper3", {
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 10,
+        speed: 8000,
+        autoplay: { delay: 0 },
+    });
+});
